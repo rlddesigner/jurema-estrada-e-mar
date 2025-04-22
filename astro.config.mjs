60ff8effe9546d33bf9ac31869f516ae0a194aa2
Suh <rlddesignerr@gmail.com>
2025-04-22 16:59:28 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
-import { defineConfig } from 'astro/config';
-export default defineConfig({});
+import { defineConfig } from "astro/config";
+import content from "@astrojs/content";
+
+export default defineConfig({
+  integrations: [content()]
+});
